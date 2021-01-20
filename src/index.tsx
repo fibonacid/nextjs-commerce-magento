1b4b0b62c89062e8f33180a99f006f078501b525
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-20 14:18:50 +0100

    feat: use fetcher from vercel example
    https://github.com/vercel/commerce-framework/blob/master/example%20/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,46 @@ import {
   CommerceProvider as CoreCommerceProvider,
   useCommerce as useCoreCommerce,
 } from "./commerce";
+import { FetcherError } from "./commerce/utils/errors";
+
+async function getText(res: Response) {
+  try {
+    return (await res.text()) || res.statusText;
+  } catch (error) {
+    return res.statusText;
+  }
+}
+
+async function getError(res: Response) {
+  if (res.headers.get("Content-Type")?.includes("application/json")) {
+    const data = await res.json();
+    return new FetcherError({ errors: data.errors, status: res.status });
+  }
+  return new FetcherError({ message: await getText(res), status: res.status });
+}
 
 export type MagentoConfig = Partial<CommerceConfig>;
 
 export const magentoConfig: CommerceConfig = {
   locale: "",
   cartCookie: "",
-  async fetcher() {},
+  async fetcher({ url, method = "GET", variables, body: bodyObj }) {
+    const hasBody = Boolean(variables || bodyObj);
+    const body = hasBody
+      ? JSON.stringify(variables ? { variables } : bodyObj)
+      : undefined;
+    const headers = hasBody
+      ? { "Content-Type": "application/json" }
+      : undefined;
+    const res = await fetch(url!, { method, body, headers });
+
+    if (res.ok) {
+      const { data } = await res.json();
+      return data;
+    }
+
+    throw await getError(res);
+  },
 };
 
 export type MagentoProps = {
