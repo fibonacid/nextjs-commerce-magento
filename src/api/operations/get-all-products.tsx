75cc492befeb021e5211d8d045dba1c7895066b3
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-20 18:46:59 +0100

    feat: add getAllProducts function with mock data

--- src/api/operations/get-all-products.tsx
+++ src/api/operations/get-all-products.tsx
@@ -1,3 +1,4 @@
+import { GetAllProductsQueryVariables, ProductsFragment } from "src/schema";
 import { productsFragment } from "../fragments/products";
 
 export const getAllProductsQuery = /* GraphQL */ `
@@ -20,3 +21,19 @@ export const getAllProductsQuery = /* GraphQL */ `
   }
   ${productsFragment}
 `;
+
+async function getAllProducts({
+  query = getAllProductsQuery,
+  variables,
+}: {
+  query?: String;
+  variables?: GetAllProductsQueryVariables;
+}): Promise<ProductsFragment> {
+  // Return mocked data until we figure out a fetch strategy
+  return {
+    __typename: "Products",
+    items: [{ __typename: "VirtualProduct", name: "test" }],
+  };
+}
+
+export default getAllProducts;
